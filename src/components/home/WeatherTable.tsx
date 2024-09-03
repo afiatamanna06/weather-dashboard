f786ac79154e9a4bfac99f955cac059ff7775a29
afiatamanna06 <afiatamanna06@gmail.com>
2024-09-03 15:46:53 +0600

    Update home page

--- src/components/home/WeatherTable.tsx
+++ src/components/home/WeatherTable.tsx
@@ -1,28 +1,45 @@
-import { useEffect } from "react";
-import { weatherData } from "./WeatherData";
+import { useContext, useEffect, useState } from "react";
 import { IoIosPartlySunny, IoIosSunny, IoMdRainy } from "react-icons/io";
+import { WeatherContext } from "../../utils/WeatherContext";
+
+interface WeatherData {
+    name: string;
+    temperature: number;
+    condition: string;
+    humidity: number;
+    wspeed: number;
+  };
 
 const WeatherTable = () => {
+  const city = useContext(WeatherContext)
+  const [weatherData, setWeatherData] = useState<WeatherData[]>([])
+  
   const apiKey = "d588948b013155b2b1d7e46ec17e4840";
-  const city = "London";
-  const units = "metric";
 
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${44.34}&lon=${10.99}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   useEffect(() => {
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
         if (data.cod === 200) {
-          console.log(
-            `Weather in ${data.name}: ${data.weather[0].description}`
-          );
-          console.log(`Temperature: ${data.main.temp}°C`);
+          const receivedData = {
+            name: data.name,
+            temperature: data.main.temp,
+            condition: data.weather[0].description,
+            humidity: data.main.humidity,
+            wspeed: data.wind.speed,
+          }
+          console.log(receivedData)
+          if (!(weatherData.some((item) => item.name === receivedData.name))) {
+            weatherData.push(receivedData);
+          }
+          console.log(weatherData)
         } else {
           console.error(`Error: ${data.message}`);
         }
       })
       .catch((error) => console.error("Error fetching weather data:", error));
-  }, [city, units, apiKey, apiUrl]);
+  }, [apiKey, apiUrl, weatherData]);
   return (
     <div className="text-gray-800 max-h-[100%] overflow-y-auto w-full rounded-lg px-5  bg-slate-50">
       <div className="w-full overflow-x-auto">
@@ -34,7 +51,7 @@ const WeatherTable = () => {
                   scope="col"
                   className="py-3.5 pl-4 pr-3 text-left text-lg font-medium text-gray-500 sm:pl-0"
                 >
-                  City Name
+                  City Name 
                 </th>
                 <th
                   scope="col"
@@ -69,16 +86,16 @@ const WeatherTable = () => {
                     {item.name}
                   </td>
                   <td className="whitespace-nowrap px-3 py-4 text-gray-500">
-                    {item.temperature}
+                    {item.temperature}°F
                   </td>
                   <td className="whitespace-nowrap px-3 py-4 text-gray-500">
-                    {item.condition === "sunny" && (
+                    {item.condition === "clear sky" && (
                       <IoIosSunny size={30} color="yellow" />
                     )}
-                    {item.condition === "cloudy" && (
+                    {(item.condition === "overcast clouds" || item.condition === "haze" || item.condition === "broken clouds") && (
                       <IoIosPartlySunny size={30} color="gray" />
                     )}
-                    {item.condition === "rainy" && (
+                    {(item.condition === "heavy intensity rain" || item.condition === "light rain") && (
                       <IoMdRainy size={30} color="skyblue" />
                     )}
                   </td>
@@ -88,21 +105,6 @@ const WeatherTable = () => {
                   <td className="whitespace-nowrap px-3 py-4 text-gray-500">
                     {item.wspeed}
                   </td>
-
-                  {/* <td className="whitespace-nowrap px-3 py-4">
-                    {item.condition === "Delivered" && (
-                      <Badge color="emerald">{item.condition}</Badge>
-                    )}
-                    {item.condition === "In transit" && (
-                      <Badge color="blue">{item.condition}</Badge>
-                    )}
-                    {item.condition === "Pending" && (
-                      <Badge color="rose">{item.condition}</Badge>
-                    )}
-                    {item.condition === "Paid" && (
-                      <Badge color="amber">{item.condition}</Badge>
-                    )}
-                  </td> */}
                 </tr>
               ))}
             </tbody>
--- src/components/navigation/SideBar.tsx
+++ src/components/navigation/SideBar.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { routes } from "../../routes/RouteList";
 import { Link, useLocation } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
+import { WeatherContext } from "../../utils/WeatherContext";
 
 const SideBar = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
+  const [weather, setWeather] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  const handleKeyDown = (e: { key: string; }) => {
+    if (e.key === "Enter") {
+      setWeather(inputValue);
+    }
+  };
   return (
     <div className="flex w-full">
       <div className="flex flex-col h-screen fixed w-[4rem] md:w-[8rem] p-3 bg-slate-100">
@@ -31,14 +40,19 @@ const SideBar = ({ children }: { children: React.ReactNode }) => {
             </div>
             <input
               type="text"
-              name="text"
-              id="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={handleKeyDown}
               className="w-[8.7rem] md:w-[16rem] py-2 rounded-md border-0 ring-0 outline-0 focus:ring-0 pl-8 md:pl-10"
               placeholder="Search here"
             />
           </div>
         </div>
-        <div className="mt-[4.5rem] ml-2 mr-2">{children}</div>
+        <div className="mt-[4.5rem] ml-2 mr-2">
+          <WeatherContext.Provider value={weather}>
+            {children}
+          </WeatherContext.Provider>
+        </div>
       </div>
     </div>
   );
